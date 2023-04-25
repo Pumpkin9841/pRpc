93cff644d8e836a192b43af763440d1a8ce5d47d
zhoufan1 <zhoufan1@xiaohongshu.com>
2023-04-25 21:29:32 +0800

    feat add Server

---
+++ prpc-core/src/main/java/org/example/core/common/cache/CommonServerCache.java
@@ -0,0 +1,12 @@
+package org.example.core.common.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author zhoufan
+ * @create 2023/4/25
+ */
+public class CommonServerCache {
+    public static final Map<String,Object> PROVIDER_CLASS_MAP = new HashMap<>();
+}
