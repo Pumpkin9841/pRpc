7783b78e7469b89e4239a63a3f350a64542ea792
zhoufan1 <zhoufan1@xiaohongshu.com>
2023-05-05 13:44:46 +0800

    fix 完善ConnectoinHandler

--- prpc-core/src/main/java/org/example/core/client/ConnectionHandler.java
+++ prpc-core/src/main/java/org/example/core/client/ConnectionHandler.java
@@ -3,6 +3,13 @@ package org.example.core.client;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import lombok.Data;
+import org.example.core.common.ChannelFutureWrapper;
+import org.example.core.common.cache.CommonClientCache;
+import org.example.core.common.utils.CommonUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -18,8 +25,67 @@ public class ConnectionHandler {
      */
     private static Bootstrap bootstrap;
 
+    public static void setBootstrap(Bootstrap bootstrap) {
+        ConnectionHandler.bootstrap = bootstrap;
+    }
+
+    /**
+     * 构建单个连接通道
+     * 既要处理连接，还要统一将连接进行内存存储管理
+     * @param providerServiceName
+     * @param providerIp
+     * @throws InterruptedException
+     */
+    public static void connect(String providerServiceName, String providerIp) throws InterruptedException {
+        if(bootstrap == null) {
+            throw new RuntimeException("bootstrap can not be null");
+        }
+        //格式错误
+        if (!providerIp.contains(":")) {
+            return;
+        }
+        String[] providerAddress = providerIp.split(":");
+        String ip = providerAddress[0];
+        Integer port = Integer.valueOf(providerAddress[1]);
+        //TODO ChannelFuture里面到底是什么
+        ChannelFuture channelFuture = bootstrap.connect(ip, port).sync();
+        ChannelFutureWrapper channelFutureWrapper = new ChannelFutureWrapper();
+        channelFutureWrapper.setChannelFuture(channelFuture);
+        channelFutureWrapper.setHost(ip);
+        channelFutureWrapper.setPort(port);
+        CommonClientCache.SERVER_ADDRESS.add(providerIp);
+        List<ChannelFutureWrapper> channelFutureWrappers = CommonClientCache.CONNECT_MAP.get(providerServiceName);
+        if(CommonUtils.isEmptyList(channelFutureWrappers)) {
+            channelFutureWrappers = new ArrayList<>();
+        }
+        channelFutureWrappers.add(channelFutureWrapper);
+        CommonClientCache.CONNECT_MAP.put(providerServiceName, channelFutureWrappers);
+    }
+
+    /**
+     * 构建ChannelFuture
+     * @param ip
+     * @param port
+     * @return
+     * @throws InterruptedException
+     */
     public static ChannelFuture createChannelFuture(String ip, Integer port) throws InterruptedException {
         ChannelFuture channelFuture = bootstrap.connect(ip, port).sync();
+        return channelFuture;
+    }
+
+    /**
+     * 获取ChannelFuture
+     * 默认走随机策略
+     * @param providerServiceName
+     * @return
+     */
+    public static ChannelFuture getChannelFuture(String providerServiceName) {
+        List<ChannelFutureWrapper> channelFutureWrappers = CommonClientCache.CONNECT_MAP.get(providerServiceName);
+        if(CommonUtils.isNotEmptyList(channelFutureWrappers)) {
+            throw new RuntimeException("no provider exits for " + providerServiceName);
+        }
+        ChannelFuture channelFuture = channelFutureWrappers.get(new Random().nextInt(channelFutureWrappers.size())).getChannelFuture();
         return channelFuture;
     }
 }
