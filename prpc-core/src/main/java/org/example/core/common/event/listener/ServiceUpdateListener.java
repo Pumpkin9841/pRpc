fa3de15a2806f17f2be57ee882056029f4077c9d
zhoufan1 <zhoufan1@xiaohongshu.com>
2023-05-05 13:09:22 +0800

    feat ServiceUpdateListener 添加注释

--- prpc-core/src/main/java/org/example/core/common/event/listener/ServiceUpdateListener.java
+++ prpc-core/src/main/java/org/example/core/common/event/listener/ServiceUpdateListener.java
@@ -27,21 +27,28 @@ public class ServiceUpdateListener implements PRpcListener<PRpcUpdateEvent> {
     @Override
     public void callBack(Object t) {
         URLChangeWrapper urlChangeWrapper = (URLChangeWrapper) t;
+        //获取与服务名称关联的ChannelFutureWrapper列表
         List<ChannelFutureWrapper> channelFutureWrappers = CommonClientCache.CONNECT_MAP.get(urlChangeWrapper.getServiceName());
 
+        //判断获取到的ChannelFutureWrapper列表是否为空，为空则记录error日志并返回
         if(CommonUtils.isEmptyList(channelFutureWrappers)) {
             log.error("[ServiceUpdateListener] channelFutureWrappers is empty");
             return;
         } else {
+            //获取新的服务提供者URL列表
             List<String> matchProviderUrl = urlChangeWrapper.getProviderUrl();
+            //存放已处理过的URL
             Set<String> finalUrl = new HashSet<>();
+            //创建一个用于存放最终ChannelFutureWrapper的列表
             List<ChannelFutureWrapper> finalChannelFutureWrappers = new ArrayList<>();
-            for (ChannelFutureWrapper channelFutureWrapper : finalChannelFutureWrappers) {
+            for (ChannelFutureWrapper channelFutureWrapper : channelFutureWrappers) {
                 String oldServerAddress = channelFutureWrapper.getHost() + ":" + channelFutureWrapper.getPort();
                 //如果旧的url没有，说明已经被移除了
                 if(!matchProviderUrl.contains(oldServerAddress)) {
+                    //如果不存在，则跳过此次循环
                     continue;
                 } else {
+                    //如果存在，则将当前ChannelFutureWrapper添加到最终列表中，并将对应的URL添加到已经处理过的URL集合中
                     finalChannelFutureWrappers.add(channelFutureWrapper);
                     finalUrl.add(oldServerAddress);
                 }
@@ -49,6 +56,7 @@ public class ServiceUpdateListener implements PRpcListener<PRpcUpdateEvent> {
             //此时的旧url已经被移除了，开始检查是否有新的url
             ArrayList<ChannelFutureWrapper> newChannelFutureWrapper = new ArrayList<>();
             for (String newProviderUrl : matchProviderUrl) {
+                //判断当前URL是否已经处理过了
                 if(!finalUrl.contains(newProviderUrl)) {
                     ChannelFutureWrapper channelFutureWrapper = new ChannelFutureWrapper();
                     String host = newProviderUrl.split(":")[0];
@@ -66,6 +74,7 @@ public class ServiceUpdateListener implements PRpcListener<PRpcUpdateEvent> {
                     }
                 }
             }
+            //将新建立的连接添加到最终列表中
             finalChannelFutureWrappers.addAll(newChannelFutureWrapper);
             //最终更新服务
             CommonClientCache.CONNECT_MAP.put(urlChangeWrapper.getServiceName(), finalChannelFutureWrappers);
